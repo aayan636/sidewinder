4c4278ec57f5a52a3dd5d6f6e59b1905054f6e25
aayan636 <aayan.kumar123@gmail.com>
2026-03-29 12:23:52 -0700

    for:

--- src/analysis/ast/transformer.py
+++ src/analysis/ast/transformer.py
@@ -257,7 +257,8 @@ class SidewinderTransformer(ast.NodeTransformer):
         self.current_scope.append(node.name)
         
         # Transform class body
-        node.body = self._visit_list_of_stmts(node.body)
+        with self.current_context.enter_context(node, "body") as c:
+            node.body = self._visit_list_of_stmts(node.body)
         
         # Transform decorators
         node.decorator_list = [self._visit_expr(dec) for dec in node.decorator_list]
@@ -309,7 +310,7 @@ class SidewinderTransformer(ast.NodeTransformer):
             self._visit_target(node.target, self._visit_expr(node.value))
         return None
     
-    def visit_AugAssign(self, node: ast.AugAssign) -> Any:
+    def visit_AugAssign(self, node: ast.AugAssign) -> None:
         op_map = {
             ast.Add:      'add',
             ast.Sub:      'sub',
@@ -360,7 +361,7 @@ class SidewinderTransformer(ast.NodeTransformer):
         self._visit_target(node.target, ast.Name(id=tmp, ctx=ast.Load()))
 
     
-    def visit_For(self, node: ast.For) -> Any:
+    def visit_For(self, node: ast.For) -> list[ast.stmt]:
         """
         Transform for loop to explicit iterator protocol.
         
@@ -368,71 +369,20 @@ class SidewinderTransformer(ast.NodeTransformer):
             body
         
         Becomes:
-        __iter = iterable.__sidewinder_iter__(__sidewinder_state)
+        _iter = iterable.__sidewinder_iter__(__sidewinder_state)
         while True:
             try:
-                item = __iter.__sidewinder_next__(__sidewinder_state)
+                _next_val = _iter.__sidewinder_next__(__sidewinder_state)
+                item = _next_val  # via _visit_target, handles tuple unpack etc.
                 body
             except StopIteration:
                 break
         """
-        iter_var = self._fresh_temp("__iter")
-        
-        # Create: __iter = iterable.__sidewinder_iter__(__sidewinder_state)
-        iter_assign = ast.Assign(
-            targets=[ast.Name(id=iter_var, ctx=ast.Store())],
-            value=ast.Call(
-                func=ast.Attribute(
-                    value=self.visit(node.iter),
-                    attr='__sidewinder_iter__',
-                    ctx=ast.Load()
-                ),
-                args=[ast.Name(id='__sidewinder_state', ctx=ast.Load())],
-                keywords=[]
-            )
-        )
-        
-        # Create: item = __iter.__sidewinder_next__(__sidewinder_state)
-        next_assign = ast.Assign(
-            targets=[node.target],
-            value=ast.Call(
-                func=ast.Attribute(
-                    value=ast.Name(id=iter_var, ctx=ast.Load()),
-                    attr='__sidewinder_next__',
-                    ctx=ast.Load()
-                ),
-                args=[ast.Name(id='__sidewinder_state', ctx=ast.Load())],
-                keywords=[]
-            )
-        )
-        
-        # Transform loop body
-        body = [self.visit(stmt) for stmt in node.body]
-        
-        # Create the try-except block
-        try_block = ast.Try(
-            body=[next_assign] + body,
-            handlers=[
-                ast.ExceptHandler(
-                    type=ast.Name(id='StopIteration', ctx=ast.Load()),
-                    name=None,
-                    body=[ast.Break()]
-                )
-            ],
-            orelse=[],
-            finalbody=[]
-        )
-        
-        # Create the while True loop
-        while_loop = ast.While(
-            test=ast.Constant(value=True),
-            body=[try_block],
-            orelse=[self.visit(stmt) for stmt in node.orelse] if node.orelse else []
-        )
-        
-        return [iter_assign, while_loop]
-    
-    def visit_AsyncFor(self, node: ast.AsyncFor) -> Any:
+        if node.orelse:
+            raise NotImplementedError("for/else not supported")
+        return self._transform_for(node.iter, node.target, node.body, is_async=False)
+
+    def visit_AsyncFor(self, node: ast.AsyncFor) -> list[ast.stmt]:
         """
         Transform async for loop to explicit async iterator protocol.
         
@@ -440,72 +390,96 @@ class SidewinderTransformer(ast.NodeTransformer):
             body
         
         Becomes:
-        __aiter = iterable.__sidewinder_aiter__(__sidewinder_state)
+        _aiter = iterable.__sidewinder_aiter__(__sidewinder_state)
         while True:
             try:
-                item = await __aiter.__sidewinder_anext__(__sidewinder_state)
+                _next_val = await _aiter.__sidewinder_anext__(__sidewinder_state)
+                item = _next_val  # via _visit_target, handles tuple unpack etc.
                 body
             except StopAsyncIteration:
                 break
         """
-        # TODO: Similar to visit_For but with __aiter__/__anext__ and await
-        aiter_var = self._fresh_temp("__aiter")
-        
-        # Create: __aiter = iterable.__sidewinder_aiter__(__sidewinder_state)
-        aiter_assign = ast.Assign(
-            targets=[ast.Name(id=aiter_var, ctx=ast.Store())],
+        if node.orelse:
+            raise NotImplementedError("async for/else not supported")
+        return self._transform_for(node.iter, node.target, node.body, is_async=True)
+    
+    def _transform_for(
+        self,
+        iter_expr: ast.expr,
+        target: ast.expr,
+        body: list[ast.stmt],
+        is_async: bool,
+    ) -> list[ast.stmt]:
+        iter_method  = '__sidewinder_aiter__' if is_async else '__sidewinder_iter__'
+        next_method  = '__sidewinder_anext__' if is_async else '__sidewinder_next__'
+        stop_exc     = 'StopAsyncIteration'   if is_async else 'StopIteration'
+        iter_tmp     = self._fresh_temp("__aiter" if is_async else "__iter")
+
+        # _iter = iterable.__sidewinder_iter__(__sidewinder_state)
+        iter_assign = ast.Assign(
+            targets=[ast.Name(id=iter_tmp, ctx=ast.Store())],
             value=ast.Call(
                 func=ast.Attribute(
-                    value=self.visit(node.iter),
-                    attr='__sidewinder_aiter__',
-                    ctx=ast.Load()
+                    value=self._visit_expr(iter_expr),
+                    attr=iter_method,
+                    ctx=ast.Load(),
                 ),
                 args=[ast.Name(id='__sidewinder_state', ctx=ast.Load())],
-                keywords=[]
-            )
+                keywords=[],
+            ),
+            lineno=0, col_offset=0,
         )
-        
-        # Create: item = await __aiter.__sidewinder_anext__(__sidewinder_state)
+
+        # _next_val = _iter.__sidewinder_next__(__sidewinder_state)
+        # (possibly wrapped in await)
+        next_call = ast.Call(
+            func=ast.Attribute(
+                value=ast.Name(id=iter_tmp, ctx=ast.Load()),
+                attr=next_method,
+                ctx=ast.Load(),
+            ),
+            args=[ast.Name(id='__sidewinder_state', ctx=ast.Load())],
+            keywords=[],
+        )
+        next_value = ast.Await(value=next_call) if is_async else next_call    
+        # emit next call into a fresh tmp, then assign to target via _visit_target
+        next_tmp = self._fresh_temp("__next_val")
         next_assign = ast.Assign(
-            targets=[node.target],
-            value=ast.Await(
-                value=ast.Call(
-                    func=ast.Attribute(
-                        value=ast.Name(id=aiter_var, ctx=ast.Load()),
-                        attr='__sidewinder_anext__',
-                        ctx=ast.Load()
-                    ),
-                    args=[ast.Name(id='__sidewinder_state', ctx=ast.Load())],
-                    keywords=[]
-                )
-            )
-        )
-        
-        # Transform loop body
-        body = [self.visit(stmt) for stmt in node.body]
-        
-        # Create the try-except block
+            targets=[ast.Name(id=next_tmp, ctx=ast.Store())],
+            value=next_value,
+            lineno=0, col_offset=0,
+        )   
+
+        # Create empty try block to push statements into
         try_block = ast.Try(
-            body=[next_assign] + body,
+            body=[next_assign],
             handlers=[
                 ast.ExceptHandler(
-                    type=ast.Name(id='StopAsyncIteration', ctx=ast.Load()),
+                    type=ast.Name(id=stop_exc, ctx=ast.Load()),
                     name=None,
-                    body=[ast.Break()]
+                    body=[ast.Break()],
                 )
             ],
             orelse=[],
-            finalbody=[]
+            finalbody=[],
         )
-        
-        # Create the while True loop
+
+        # transform loop body
+        with self.current_context.enter_context(try_block, "body"): 
+            # assign tmp to actual target (handles tuple unpack, attribute, subscript etc.)
+            self._visit_target(target, ast.Name(id=next_tmp, ctx=ast.Load()))
+            transformed_body = self._visit_list_of_stmts(body)
+
+        try_block.body.extend(transformed_body)
+
         while_loop = ast.While(
             test=ast.Constant(value=True),
             body=[try_block],
-            orelse=[self.visit(stmt) for stmt in node.orelse] if node.orelse else []
+            orelse=[],
+            lineno=0, col_offset=0,
         )
-        
-        return [aiter_assign, while_loop]
+
+        return [iter_assign, while_loop]
     
     def visit_While(self, node: ast.While) -> Any:
         """Transform while loop - transform condition and body."""
